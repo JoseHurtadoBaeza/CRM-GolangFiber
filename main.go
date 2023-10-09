9d7bfed87869182b62864acff1cc2f2adc484692
JoseHurtadoBaeza <josehurtadobaeza@gmail.com>
2023-10-09 05:17:22 +0200

    Start coding

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "github.com/gofiber/fiber"
+
+func setupRoutes(app *fiber.App) {
+
+	app.Get(GetLeads)
+	app.Get(GetLead)
+	app.Post(NewLead)
+	app.Delete(DeleteLead)
+
+}
+
+func main() {
+
+	app := fiber.New()
+	setupRoutes(app)
+	app.Listen(3000)
+
+}
